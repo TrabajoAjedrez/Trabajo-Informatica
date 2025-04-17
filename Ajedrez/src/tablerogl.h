38fa5b303ad993296879d06c91624d46aa440d95
Gabriel-Rpascual <61052061+Gabriel-Rpascual@users.noreply.github.com>
2025-04-17 18:21:10 +0200

    Cambios en tablero.

--- Ajedrez/src/tablerogl.h
+++ Ajedrez/src/tablerogl.h
@@ -1,22 +1,10 @@
 #pragma once
 #include "freeglut.h"
+#include "tablero.h"
 
 class TableroGL {
-public:
-    // Constructor: tamaño del tablero (normalmente 8x8) y tamaño de cada casilla
-    TableroGL(int dimension = 8, float tamCasilla = 1.0f);
-
-    // Dibuja el tablero en la posición actual
-    void dibuja();
-
-    // Establece la posición del tablero
-    void setPosicion(float x, float y, float z);
-
-    // Obtiene el tamaño total del tablero
-    float getTamanoTotal() const;
-
 private:
-    int dimension;          // Dimensión del tablero (8 para ajedrez estándar)
+
     float tamCasilla;       // Tamaño de cada casilla
     float posX, posY, posZ; // Posición del tablero
 
@@ -26,5 +14,26 @@ private:
 
     // Dibuja una casilla individual
     void dibujarCasilla(float x, float z, bool esClara);
+public:
+    // Constructor: tamaño del tablero (normalmente 8x8) y tamaño de cada casilla
+    TableroGL(float tamCasilla = 1.0f):
+        tamCasilla(tamCasilla), posX(0), posY(0), posZ(0)
+    {
+        colorClaro[0] = 0.96f; colorClaro[1] = 0.96f; colorClaro[2] = 0.86f;
+        colorOscuro[0] = 0.55f; colorOscuro[1] = 0.27f; colorOscuro[2] = 0.07f;
+    }
+
+    //TEMPORAL ESTO LUEGO LO CAMBIO
+    
+    // Dibuja el tablero en la posición actual
+    void dibuja(int filas, int columnas, float tamCasilla = 1.0f);
+
+    // Establece la posición del tablero
+    void setPosicion(float x, float y, float z);
+
+    // Obtiene el tamaño total del tablero
+    float getTamanoTotal() const;
+
+
 };
 
