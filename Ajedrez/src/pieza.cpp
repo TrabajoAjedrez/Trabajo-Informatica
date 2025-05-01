c622b3f3f883e4fc2beb1a7b0ee8157f6f8ced17
victorinox05 <victorhcb10@gmail.com>
2025-05-01 15:48:55 +0200

    Prueba

---
+++ Ajedrez/src/pieza.cpp
@@ -0,0 +1,8 @@
+#include "pieza.h"
+
+//vector<Vector2D> Pieza::obtenerMovimientosPosibles(const vector<vector<int>>& tablero)
+//{
+//	vector<Vector2D> movimientos; //La variables que se va a devolver
+//
+//
+//}
