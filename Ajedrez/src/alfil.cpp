72508ddcefdccbe21b0613ba4f0fbc6e806c7ab0
HUGO SIERRA BLAZQUEZ <hugo.sierra@alumnos.upm.es>
2025-05-20 20:08:12 +0200

    merge alfil into main

---
+++ Ajedrez/src/alfil.cpp
@@ -0,0 +1,35 @@
+#include "alfil.h"
+#include "ETSIDI.h"
+
+ClassAlfil::ClassAlfil(Color color, Vector2D posicion)
+	: ClassPieza(Pieza_t::Alfil, color, posicion)
+{
+	sprite.setCenter(1, 1);
+	sprite.setSize(0.8, 0.85);
+	sprite2.setCenter(1, 1);
+	sprite2.setSize(0.8, 0.85);
+}
+void ClassAlfil::dibuja(float x, float y) {
+	glPushMatrix();
+	glTranslated(x, y, 0.1);
+	if (color == Color::BLANCO)
+		sprite.draw();
+	else
+		sprite2.draw();
+	glPopMatrix();
+}
+
+void ClassAlfil::anima() {
+	if (color == Color::BLANCO) {
+		if (sprite.getState() == 0)
+			sprite.setState(0, false);
+		sprite.draw();
+	}
+	else {
+		if (sprite.getState() == 0)
+			sprite2.setState(0, false);
+		sprite.draw();
+	}
+	sprite.loop();
+	sprite2.loop();
+}
--- Ajedrez/src/alfil.h
+++ Ajedrez/src/alfil.h
@@ -1,5 +1,21 @@
-#pragma once
-class alfil
-{
-};
 
+#pragma once
+
+#include <vector>
+#include "Pieza.h"
+#include "Tablero.h"
+#include "ETSIDI.h"
+
+
+class ClassAlfil : public ClassPieza {
+
+	ETSIDI::SpriteSequence sprite{ "Imagenes/alfilAzul.png",4 };
+	ETSIDI::SpriteSequence sprite2{ "Imagenes/alfilRojo.png",4 };
+
+public:
+	ClassAlfil(Color color, Vector2D posicion);
+	ClassAlfil() = default;
+
+	void dibuja(float x, float y);
+	void anima();
+};
