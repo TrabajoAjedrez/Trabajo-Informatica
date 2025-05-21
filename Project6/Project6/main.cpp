bb5d0a58807742ef7b2462b9edf323f498c58905
Andrea-Castelli <andrea.castelli@alumnos.upm.es>
2025-05-21 10:55:59 +0200

    cambios en temporizador y en seleccion de variante

---
+++ Project6/Project6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+#include <cmath>
+
+using namespace std;
+
+class espada{/*...*/ };
+class persona{/*...*/ };
+
+class guerrero: public persona, private espada{
+};
+
+int main() {
+	
+	
+}
